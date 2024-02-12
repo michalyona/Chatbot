15a250808e70140b7794d9a6c031bdb6bf3096fc
michal <my0527615247@gmail.com>
2024-02-12 22:48:23 +0200

    תיקון באגים

--- Plugins/CountDown/CountDownPlugin.cs
+++ Plugins/CountDown/CountDownPlugin.cs
@@ -17,7 +17,9 @@ namespace CountDown
 
         public PluginOutput Execute(PluginInput input)
         {
-            var interval = int.Parse(input.Message);
+            var interval = 0;
+            if (input!= null&&input.Message!="")
+            interval = int.Parse(input.Message);
             _scheduler.Schedule(TimeSpan.FromSeconds(interval), Id, "");
             return new PluginOutput("Countdown started.");
 
